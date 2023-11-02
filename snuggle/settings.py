491d6a2886f3e71e700633e51b85a5280d2fa07e
bamcasa <combeesang@gmail.com>
2023-11-02 19:35:07 +0900

    FIx: 수정

--- snuggle/settings.py
+++ snuggle/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
 
 # Application definition
 
